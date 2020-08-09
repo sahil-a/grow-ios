200c9429a8803174284f92b5ca21b9ce31035103
Abhishek <abhimore74@gmail.com>
2020-08-09 15:22:34 -0400

    Welcome Header + Fonts

---
+++ grow-ios/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  grow-ios
+//
+//  Created by Abhishek More on 8/9/20.
+//  Copyright © 2020 hackthis. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ grow-ios/Views/WelcomeHeader.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  grow-ios
+//
+//  Created by Sahil Ambardekar on 8/9/20.
+//  Copyright © 2020 hackthis. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeHeader: View {
+    let name: String
+    let streak: Int
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Hello")
+                        .font(.custom("Cabin-Bold", size: 24))
+                        .foregroundColor(Color(red: 64/255, green: 100/255, blue: 25/255)) +
+                    Text(", \(name)")
+                        .font(.custom("Cabin-Bold", size: 24))
+                }
+                Text("You're on a \(streak) day streak!")
+                    .font(.custom("Cabin-Bold", size: 30))
+            }
+        }
+    }
+}
+
+struct WelcomeHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeHeader(name: "Abhishek", streak: 5)
+    }
+}
