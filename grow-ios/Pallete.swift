ccdf442a3942ecfbd2f23abd7fc76d44663a7179
Abhishek <abhimore74@gmail.com>
2020-08-10 23:27:08 -0400

    Color Pallete

---
+++ grow-ios/Pallete.swift
@@ -0,0 +1,16 @@
+//
+//  Pallete.swift
+//  grow-ios
+//
+//  Created by Abhishek More on 8/10/20.
+//  Copyright © 2020 hackthis. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let offwhite = Color(red: 225/255, green: 225/255, blue: 235/255)
+    static let darkStart = Color(red: 50/255, green: 60/255, blue: 65/255)
+    static let darkEnd = Color(red: 25/255, green: 25/255, blue: 35/255)
+}
